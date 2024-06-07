87c9e3a089069eafc7cd1ccc6d32c999aa2dbcb9
Bruno_Ss <brunosch250@gmail.com>
2024-06-07 15:47:58 -0300

    Update cards

--- memory-project-app/src/app/components/item-card/item-card.component.ts
+++ memory-project-app/src/app/components/item-card/item-card.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import * as fs from 'fs';
-import * as path from 'path';
+// import * as fs from 'fs';
+// import * as path from 'path';
 
 @Component({
   selector: 'app-item-card',
@@ -15,43 +15,53 @@ export class ItemCardComponent {
   itemCards: Array<itemCard> = [
     {
       img: '../../../assets/imgs/goodKids.png',
-      dsc: 'GoodKids',
+      tlt: 'Good Kid',
+      dsc: 'Uma banda conhecida por transformar sons de Jazz em música, vida e arte.',
       alt: 'uma banda muito legal!'
+    },
+    {
+      img: '../../../assets/imgs/Meghan-Trainor.jpg',
+      tlt: 'Meghan Trainor',
+      dsc: 'Meghan Elizabeth Trainor é uma cantora, compositora e produtora norte-americana.',
+      alt: 'Ela cantou em Smurfs!'
     },
   ];
 
   constructor() {
-    const imgs = this.getItemCardsImg();
+    // const imgs = this.getItemCardsImg();
   }
 
   /** Exibe os cards se há os dados */
   public getItemCards() {
-    this.itemCards.forEach(item => {
-      if(!item.dsc || !item.img || !item.alt) return false;
-      return true;
-    });
+    if(this.itemCards.length >= 1) return true;
+    return false;
+    // this.itemCards.forEach(item => {
+    //   if(item.img && item.alt && item.dsc) return true;
+    //   return false;
+    // });
   }
 
-  public getItemCardsImg(): string[]{
-    let folderPath = '../../../assets/imgs/';
-    try {
-      const files = fs.readdirSync(folderPath);
-      const imageFiles = files.filter((file) => {
-          const extension = path.extname(file).toLowerCase();
-          return ['.jpg', '.jpeg', '.png', '.gif'].includes(extension);
-      });
-      return imageFiles;
-    } catch (error) {
-        console.error('Error reading folder:', error);
-        return [];
-    }
+  // public getItemCardsImg(): string[]{
+  //   let folderPath = '../../../assets/imgs/';
+  //   try {
+  //     const files = fs.readdirSync(folderPath);
+  //     const imageFiles = files.filter((file) => {
+  //         const extension = path.extname(file).toLowerCase();
+  //         return ['.jpg', '.jpeg', '.png', '.gif'].includes(extension);
+  //     });
+  //     return imageFiles;
+  //   } catch (error) {
+  //       console.error('Error reading folder:', error);
+  //       return [];
+  //   }
 
-  }
+  // }
 
 }
 
 interface itemCard {
   img: string,
+  tlt: string,
   dsc: string,
   alt: string,
 }
---
+++ memory-project-app/src/server.js
@@ -0,0 +1,19 @@
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'seu_usuario',
+//   password: 'sua_senha',
+//   database: 'seu_banco_de_dados'
+// });
+
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log('Conectado ao banco de dados!');
+// });
+
+// app.get('/api/data', function(req, res) {
+//   connection.query('SELECT * FROM sua_tabela', function(err, rows) {
+//     if (err) throw err;
+//     res.send(rows);
+//   });
+// });
